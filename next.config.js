111dfaf038011a0464a699d8e85d7130005e2a1d
user <user@users-MacBook-Pro.local>
2023-05-28 15:30:16 +0100

    added sanity and redesigns

--- next.config.js
+++ next.config.js
@@ -4,4 +4,11 @@ module.exports = {
   images: {
     domains: ["cdn.sanity.io"],
   },
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
 };
